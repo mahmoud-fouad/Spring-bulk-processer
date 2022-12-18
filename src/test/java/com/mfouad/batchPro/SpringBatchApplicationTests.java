48296398fc8cee1c69a5b69d196608eb36edcfae
Mahmoud Fuad <mahmoud.ah.fouad@gmail.com>
2022-12-18 12:53:24 +0200

    add spring boot project

---
+++ src/test/java/com/mfouad/batchPro/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfouad.batchPro;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
